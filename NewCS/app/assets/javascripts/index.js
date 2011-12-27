883599709d326b675d4ee419cc1f1af8b76de52d
Dmitry Krivenko <dmitrykrivenko@gmail.com>
2011-12-27 11:29:55 +0000

    Fixed bug in top scroll

--- NewCS/app/assets/javascripts/index.js
+++ NewCS/app/assets/javascripts/index.js
@@ -1,36 +1,4 @@
-(function(jq) {
-    jq.autoScroll = function(ops) {
-        ops = ops || {};
-        ops.styleClass = ops.styleClass || 'scroll-to-top-button';
-        var t = jq('<div class="'+ops.styleClass+'"></div>'),
-        d = jq(ops.target || document);
-        jq(ops.container || 'body').append(t);
 
-        t.css({
-            opacity: 0,
-            position: 'absolute',
-            top: 0,
-            right: 0
-        }).click(function() {
-            jq('html,body').animate({
-                scrollTop: 0
-            }, ops.scrollDuration || 1000);
-        });
-
-        d.scroll(function() {
-            var sv = d.scrollTop();
-            if (sv < 10) {
-                t.clearQueue().fadeOut(ops.hideDuration || 200);
-                return;
-            }
-
-            t.css('display', '').clearQueue().animate({
-                top: sv,
-                opacity: 0.8
-            }, ops.showDuration || 500);
-        });
-    };
-})(jQuery);
       
 function theRotator() {
     // Устанавливаем прозрачность всех картинок в 0
@@ -78,12 +46,6 @@ function rotate() {
 $(document).ready(function() {		
     // Запускаем слайдшоу
     theRotator();
-        
-    $.autoScroll({
-        scrollDuration: 2000, 
-        showDuration: 600, 
-        hideDuration: 300
-    }); 
 });
       
   
